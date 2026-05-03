98126e12dff6e7e3fe1c6cad14bbb0a44c3e486c
vinod kumar <vinodrao835@gmail.com>
2026-05-03 12:28:16 +0530

    push backend image to dockerhub

--- apps/backend/src/index.ts
+++ apps/backend/src/index.ts
@@ -1,5 +1,5 @@
 import express from "express";
-// import prisma from "@repo/db/client";
+import prisma from "@repo/db/client";
 
 const app = express();
 app.use(express.json());
@@ -8,29 +8,29 @@ app.get("/", (req, res) => {
   res.send("Backend server");
 });
 
-// app.get("/users", async (req, res) => {
-//   const users = await prisma.user.findMany();
+app.get("/users", async (req, res) => {
+  const users = await prisma.user.findMany();
 
-//   return res.json({
-//     msg: "Get data",
-//     users,
-//   });
-// });
+  return res.json({
+    msg: "Get data",
+    users,
+  });
+});
 
-// app.post("/user", async (req, res) => {
-//   const { email, name } = req.body;
-//   const user = await prisma.user.create({
-//     data: {
-//       email,
-//       name,
-//     },
-//   });
+app.post("/user", async (req, res) => {
+  const { email, name } = req.body;
+  const user = await prisma.user.create({
+    data: {
+      email,
+      name,
+    },
+  });
 
-//   return res.json({
-//     msg: "Post data",
-//     user,
-//   });
-// });
+  return res.json({
+    msg: "Post data",
+    user,
+  });
+});
 
 // by default bind to localhost, so it only listens to the traffic coming from your machine or ec2 machine
 // if this app present inside the docker container of vm then it only listen localhost of vm machine
